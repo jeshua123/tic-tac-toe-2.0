b51fbcbaeca14d9326a274f1baece5ff1d27b4fc
jeshua123 <jeshuaestevez1@gmail.com>
2025-09-05 02:00:11 -0400

    first commit

---
+++ tic-tac-toe-2/src/index.js
@@ -0,0 +1,12 @@
+
+import ReactDOM from 'react-dom/client';
+import ticktacktoe from './ticktacktoe';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <ticktacktoe />
+ 
+);
+
+
---
+++ tic-tac-toe-2/src/ticktacktoe.js
@@ -0,0 +1,12 @@
+function ticktacktoe() {
+  console.log("hola")
+  return (
+    <div className="main">
+      <header className="header">
+        <h1>hola mundo </h1>
+      </header>
+    </div>
+  );
+}
+
+export default ticktacktoe;
