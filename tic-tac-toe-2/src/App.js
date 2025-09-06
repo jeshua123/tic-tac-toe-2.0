d2c86ae5933d500bb70072927a780cc530eefb7d
jeshua123 <jeshuaestevez1@gmail.com>
2025-09-06 03:47:56 -0400

    0347 am 06-09-2025

--- tic-tac-toe-2/src/App.js
+++ tic-tac-toe-2/src/App.js
@@ -14,12 +14,23 @@ function App() {
            <button class="button-option"></button>
            <button class="button-option"></button>
        </div>
+       <div className="submenu"> 
+       <button id="restart">jugador 1</button>
+       <button id="restart">jugador 2</button>
+       <button id="restart">inicio</button>
        <button id="restart">Restart</button>
+       </div>
    </div>
    <div class="popup hide">
        <p id="message">TICK TACK TOE</p>
        <button id="juego_nuevo">Jugador VS jugador </button>
        <button id="dos_jugadores">Jugador vs Computador</button>
+       <button id="vs_computadora">Empezar</button>
+   </div>
+          
+   <div class="popup2 hide">
+       <input class="inputJugador" placeholder="Jugador 1"/>       
+       <input class="inputJugador" placeholder="Jugador 2"/>       
        <button id="vs_computadora">Empezar</button>
    </div>
           
